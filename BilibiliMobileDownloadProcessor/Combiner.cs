4f055aaddbead6ded1e3d622892aa51e6fed267f
Tyler Parret True <mysteryworldgod@outlook.com>
2026-06-18 18:50:15 +0800

    直接反序列化

--- BilibiliMobileDownloadProcessor/Combiner.cs
+++ BilibiliMobileDownloadProcessor/Combiner.cs
@@ -7,25 +7,40 @@ using System.Xml.Linq;
 namespace BilibiliMobileDownloadProcessor {
 	internal static class Combiner {
 
-		public struct Item {
-			public long OwnerId;
-			public string? OwnerName;
+		internal struct BiliEntry() {
+			public string title = "";
+			public string type_tag = ""; // long video_quality; long prefered_video_quality;
 
-			public long AvId;
-			public string? BvId;
-			public string? Title;
+			public long avid = 0;
+			public long season_id = 0;
+			public string bvid = "";
 
-			public long PartNum;
-			public string? PartName;
-			public long CId;
+			public long owner_id = 0;
+			public string owner_name = "";
 
-			public bool isEp;
+			public struct EpData() { // 分P （旧）
+				public long danmaku = 0; // = page_data.cid
+				public string index = "";
+				public string index_title = "";
+				public long episode_id = 0;
+			}
+			public EpData? ep = null;
 
-			public FilePath VideoPath;
-			public FilePath AudioPath;
-			public FilePath? CoverPath;
+			public struct PageData() { // 合集 （新）
+				public long cid = 0;
+				public long page = 0;
+				public string part = "";
+			}
+			public PageData? page_data = null;
+
+			/**
+			 *  "link": "bilibili://video/{avid}?cid={cid}",
+			*/
+
+			public FilePath VideoPath = new();
+			public FilePath AudioPath = new();
+			public FilePath? CoverPath = null;
 		}
-
 
 		public static void Process(string path) {
 			if (!Path.Exists(path)) {
@@ -52,31 +67,44 @@ namespace BilibiliMobileDownloadProcessor {
 			}
 		}
 
-		internal static string? Combine(Item item, FilePath path) {
+		internal static string? Combine(BiliEntry item, FilePath path) {
 			var finalpath = path / "bilibili";
 			//if (item.OwnerId != 0 || !string.IsNullOrEmpty(item.OwnerName)) {
-			finalpath /= $"[{item.OwnerId}]{item.OwnerName}";
+			finalpath /= $"[{item.owner_id}]{item.owner_name}";
 			//}
 			string work_name;
 			string part_name;
-			if (item.isEp) {
-				work_name = item.Title ?? "_";
-				part_name = $"{item.PartNum:D3}.{item.PartName}";
+			string part_filename;
+			long cid;
+			if (item.ep is BiliEntry.EpData ep) {
+				work_name = $"[{item.season_id}]{item.title}";
+				long index = long.Parse(ep.index);
+				part_name = ep.index_title;
+				part_filename = $"{index:D3}.{part_name}";
+				cid = ep.danmaku;
+			}
+			else if (item.page_data is BiliEntry.PageData pd) {
+				if (string.IsNullOrEmpty(item.bvid))
+					work_name = $"[{item.avid}]{pd.part}";
+				else
+					work_name = $"[{item.avid}]{item.title}";
+				part_name = pd.part;
+				part_filename = $"{pd.page:D3}";
+				cid = pd.cid;
 			}
 			else {
-				work_name = $"[{item.AvId}]{((item.BvId != null) ? item.Title : item.PartName)}";
-				part_name = $"{item.PartNum:D3}";
+				throw new Exception("ep and page_data are both null.");
 			}
 			if (work_name.Length > 81) {
 				work_name = work_name[..80];
 				work_name += "…";
 			}
-			if (part_name.Length > 81) {
-				part_name = part_name[..80];
-				part_name += "…";
+			if (part_filename.Length > 81) {
+				part_filename = part_filename[..80];
+				part_filename += "…";
 			}
 			finalpath /= work_name;
-			finalpath /= $"{part_name}.mp4";
+			finalpath /= $"{part_filename}.mp4";
 
 			PathHelper.EnsureFileCanExsist(finalpath);
 
@@ -88,10 +116,10 @@ namespace BilibiliMobileDownloadProcessor {
 						"-i", item.CoverPath.Path,
 						"-map", "0:v:0", "-map", "1:a:0", "-map", "2",
 						"-c:v", "copy", "-c:a", "copy",
-						"-metadata", $"title=\"{item.PartName}\"",
-						"-metadata", $"album=\"{item.Title}\"",
-						"-metadata", $"artist=\"{item.OwnerName}\"",
-						"-metadata", $"description=\"avid:{item.AvId},bvid:{item.BvId},owner_id:{item.OwnerId},cid:{item.CId}\"",
+						"-metadata", $"title=\"{part_name}\"",
+						"-metadata", $"album=\"{item.title}\"",
+						"-metadata", $"artist=\"{item.owner_name}\"",
+						"-metadata", $"description=\"avid:{item.avid},bvid:{item.bvid},owner_id:{item.owner_id},cid:{cid}\"",
 						"-disposition:2", "attached_pic",
 						"-movflags", "+faststart", "-y",
 						finalpath.Path,
@@ -103,10 +131,10 @@ namespace BilibiliMobileDownloadProcessor {
 						"-i", item.AudioPath.Path,
 						"-map", "0:v:0", "-map", "1:a:0",
 						"-c:v", "copy", "-c:a", "copy",
-						"-metadata", $"title=\"{item.PartName}\"",
-						"-metadata", $"album=\"{item.Title}\"",
-						"-metadata", $"artist=\"{item.OwnerName}\"",
-						"-metadata", $"description=\"avid:{item.AvId},bvid:{item.BvId},owner_id:{item.OwnerId},cid:{item.CId}\"",
+						"-metadata", $"title=\"{part_name}\"",
+						"-metadata", $"album=\"{item.title}\"",
+						"-metadata", $"artist=\"{item.owner_name}\"",
+						"-metadata", $"description=\"avid:{item.avid},bvid:{item.bvid},owner_id:{item.owner_id},cid:{cid}\"",
 						"-movflags", "+faststart", "-y",
 						finalpath.Path,
 						"-v", "warning"
@@ -129,7 +157,7 @@ namespace BilibiliMobileDownloadProcessor {
 		}
 
 
-		public static IEnumerable<Item> Scan(string[] paths) {
+		public static IEnumerable<BiliEntry> Scan(string[] paths) {
 			foreach (var path in paths) {
 				var entryFiles = Directory.EnumerateFiles(
 					path,
@@ -138,7 +166,7 @@ namespace BilibiliMobileDownloadProcessor {
 				);
 
 				foreach (var item in entryFiles) {
-					Item? res = null;
+					BiliEntry? res = null;
 					try {
 						FilePath itempath = new(item);
 						res = ReadEntry(itempath);
@@ -147,74 +175,28 @@ namespace BilibiliMobileDownloadProcessor {
 						Logger.Exception(ex, true);
 						Logger.Error($"Failed to process '{item}'.");
 					}
-					if (res is Item ret) {
+					if (res is BiliEntry ret) {
 						yield return ret;
 					}
 				}
 			}
 		}
 
-		internal static Item ReadEntry(FilePath path) {
-			// JSON 转化为 XML
-			XElement root;
-			using (var streamReader = new StreamReader(new FileStream(path.Path, FileMode.Open, FileAccess.Read))) {
-				var json = streamReader.ReadToEnd();
-				root = (JsonConvert.DeserializeXNode(json, "Root")?.Root) ?? throw new Exception($"Failed to read json.");
-			}
+		internal static BiliEntry ReadEntry(FilePath path) {
+			var json = File.ReadAllText(path.Path);
+			var entry = JsonConvert.DeserializeObject<BiliEntry>(json);
 
-			// 开始 处理 XML
-			string owner_id = root.Element("owner_id")?.Value ?? throw new Exception($"Entry info - owner_id not found.");
-			string? owner_name = root.Element("owner_name")?.Value;
-
-			string avid = root.Element("avid")?.Value ?? throw new Exception($"Entry info - avid not found.");
-			string? bvid = root.Element("bvid")?.Value;
-			string? title = root.Element("title")?.Value;
-
-			var page_data = root.Element("page_data");
-			var ep_data = root.Element("ep");
-			if (string.IsNullOrEmpty(page_data?.Value)) { page_data = null; }
-			if (string.IsNullOrEmpty(ep_data?.Value)) { ep_data = null; }
-			string? pagenum = null;
-			string? pagename = null;
-			string? pagecid = null;
-			bool isEp = false;
-			if (page_data == null && ep_data == null) {
-				throw new Exception($"Entry info - page_data AND ep not found.");
-			}
-			else if (page_data != null && ep_data != null) {
-				throw new Exception($"Entry info - page_data AND ep both exists.");
-			}
-			else {
-				if (page_data != null) {
-					pagenum = page_data.Element("page")?.Value;
-					pagename = page_data.Element("part")?.Value;
-					pagecid = page_data.Element("cid")?.Value;
-				}
-				if (ep_data != null) {
-					isEp = true;
-					pagenum ??= ep_data.Element("index")?.Value;
-					pagename ??= ep_data.Element("index_title")?.Value;
-					pagecid ??= ep_data.Element("episode_id")?.Value;
-				}
-			}
-			if (pagenum == null) {
-				throw new Exception($"Entry info - page_data-page AND ep-index not found.");
-			}
-			if (pagecid == null) {
-				throw new Exception($"Entry info - page_data-cid AND ep-episode_id not found.");
+			if (entry.ep != null && entry.page_data != null) {
+				throw new Exception("Invalid entry: both ep and page_data exist.");
 			}
 
 			// 构造 媒体文件路径
-			FilePath srcAudio, srcVideo;
+			FilePath srcAudio;
+			FilePath srcVideo;
 			FilePath? srcCover;
 			{
-				string quality = (root.Element("type_tag")?.Value) ?? throw new Exception($"Entry info - type_tag not found.");
-				if (string.IsNullOrEmpty(quality)) {
-					throw new Exception($"Entry info - quality is invalid.");
-				}
-
 				var partPath = path.Parent;
-				var srcDir = partPath / quality;
+				var srcDir = partPath / entry.type_tag;
 
 				if (!Directory.Exists(srcDir.Path)) {
 					throw new Exception($"Source directory not exists: '{srcDir}'.");
@@ -233,24 +215,10 @@ namespace BilibiliMobileDownloadProcessor {
 				srcCover = null;
 			}
 
-			return new Item {
-				OwnerId = long.Parse(owner_id),
-				OwnerName = owner_name,
-
-				AvId = long.Parse(avid),
-				BvId = bvid,
-				Title = title,
-
-				PartNum = long.Parse(pagenum),
-				PartName = pagename,
-				CId = long.Parse(pagecid),
-
-				isEp = isEp,
-
-				VideoPath = srcVideo,
-				AudioPath = srcAudio,
-				CoverPath = srcCover,
-			};
+			entry.VideoPath = srcVideo;
+			entry.AudioPath = srcAudio;
+			entry.CoverPath = srcCover;
+			return entry;
 		}
 	}
 }
