52fda5d9ab396a43b63241a299217a087599499e
Tyler Parret True <mysteryworldgod@outlook.com>
2026-06-22 08:02:51 +0800

    复制CombineRGBA

---
+++ CombineRGBA/CombineRGBA.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "Combiner.h"
+
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Info: Cmd line:" << endl;
+	for (int i = 0; i < argc; ++i) {
+		cout << "\t'" << argv[i] << "'" << endl;
+	}
+	cout << "End cmd line." << endl << endl;
+	if (argc == 2) {
+		cout << "Info: Work in '" << argv[1] << "'." << endl;
+		try {
+			if (ohms::CombineIn(argv[1])) {
+				cout << "Info: Task Succeed." << endl;
+			}
+			else {
+				cout << "Info: Task Failed." << endl;
+			}
+		}
+		catch (std::exception& e) {
+			cout << "Exception: '" << e.what() << "'." << endl;
+		}
+		catch (...) {
+			cout << "Exception: Unknown Exception." << endl;
+		}
+	}
+	else {
+		cout << "Error: Unsupported Argument." << endl;
+		cout << "Just drag one single folder and drop on the icon of this app." << endl;
+	}
+	system("pause");
+	return 0;
+}
---
+++ CombineRGBA/Combiner.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <filesystem>
+
+namespace ohms {
+
+bool CombineIn(std::filesystem::path dir);
+
+} // namespace ohms
